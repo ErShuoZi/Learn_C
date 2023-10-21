19ef86a4553c9f5c32186685b8197fb8f9145edf
liushuo <hhhmmm09009@gmail.com>
2023-10-21 10:47:31 +0800

    Initial Commit

---
+++ Learn_C/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Learn_C
+//
+//  Created by ErShuoZi on 2023/10/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
