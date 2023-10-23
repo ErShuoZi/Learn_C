e457cb758e79cc40eada168f8e31d973795fc36f
ErShuoZi <liushuo2951@gmail.com>
2023-10-23 13:43:40 +0800

    分支循环练习

--- main.c
+++ main.c
@@ -9,6 +9,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <math.h>
 
 //int main(int argc, const char * argv[]) {
 //    int age = 100;
@@ -299,6 +300,7 @@
 //        arr2[left] = arr1[left];
 //        arr2[right] = arr1[right];
 //        sleep(1);
+
 //        system("clear");//执行系统命令,清空名目
 //        left++;
 //        right--;
@@ -308,23 +310,213 @@
 //}
 
 
-int main(int argc, const char * argv[]) {
-    int i = 0;
-    char password[20];
-    for(i = 0; i < 3; i++) {
-        printf("请输入密码:>");
-        scanf("%s",password);
-        if(strcmp(password ,"123456") == 0) { //== 不能用来比较两个字符串是否相等,应该使用库函数 strcmp
-            printf("登录成功\n");
-            break;
-        } else {
-            printf("密码错误\n");
-        }
-    }
-    if(i == 3) {
-        printf("三次密码输入错误,请退出程序!\n");
-    }
-    return 0;
-}
+//int main(int argc, const char * argv[]) {
+//    int i = 0;
+//    char password[20];
+//    for(i = 0; i < 3; i++) {
+//        printf("请输入密码:>");
+//        scanf("%s",password);
+//        if(strcmp(password ,"123456") == 0) { //== 不能用来比较两个字符串是否相等,应该使用库函数 strcmp
+//            printf("登录成功\n");
+//            break;
+//        } else {
+//            printf("密码错误\n");
+//        }
+//    }
+//    if(i == 3) {
+//        printf("三次密码输入错误,请退出程序!\n");
+//    }
+//    return 0;
+//}
 
 
+//比较三个数中的最大值
+// int main() {
+// 	int a = 0;
+// 	int b = 0;
+// 	int c = 0;
+// 	scanf("%d%d%d",&a,&b,&c);
+// 	//算法实现
+// 	// a中放最大值
+// 	// c中放最小值
+// 	if(a < b) {
+// 		int temp = a;
+// 		 a = b;
+// 		 b = temp;	
+// 	} 
+// 	if(a < c) {
+// 		int temp = a;
+// 		a = c;
+// 		c = temp;
+// 	}
+// 	if(b < c) {
+// 		int temp = b;
+// 		b = c;
+// 		c = temp;
+// 	}
+// 	printf("%d %d %d",a,b,c);
+// 	
+// 	return 0;
+// }
+
+//打印1 - 100 之间所有三的倍数
+//int main() {
+//	int i = 0;
+//	for(i = 1; i <= 100; i++) {
+//		if(i % 3 == 0) {
+//			printf("%d", i);
+//		}
+//	}
+//	return 0;
+//} 
+
+//最大公约数 (辗转相除法)
+//int main() {
+//	int m = 24;
+//	int n = 18;
+//	int r = 0;
+//	while(m % n){
+//		//TODO
+//		r = m % n;
+//		m = n;
+//		n = r;
+//		
+//	} 
+//	printf("%d\n",n);
+//	 
+//	return 0;
+//} 
+
+// 打印1000年到2000年之间的闰年 
+//int main() {
+//	int year = 0;
+//	int count = 0;
+//	for(year = 1000; year <= 2000; year++) {
+//		//判断year 是否为闰年
+//		//能被4整除并且不能被100整除是闰年
+//		//能被400整除是闰年		
+//		if((year % 4 == 0 && year % 100 != 0 )|| year % 400 == 0){
+//			printf("%d ", year); 
+//			count++;
+//		}
+//	} 
+//	printf("\n一共有%d个闰年",count);
+//	return 0;
+//} 
+
+//打印素数
+//int main() {
+//	int i = 0;
+//	int count = 0;
+//	for(i = 100; i <= 200; i++) {
+//		//判断 i 是否为素数 
+//		//素数判断规则
+//		//1.试除法
+//		//产生 2 到 i - 1 的数字
+//		int j = 0;
+//
+//		for(j = 2; j < i; j++){
+//			if(	i % j == 0){
+//				//TODO
+//				break;
+//			}
+//		}
+//		if(j == i){
+//			//TODO
+//			printf("%d ",i);
+//			count++;
+//		}
+//	} 
+//	printf("\n%d",count);
+//
+//	return 0;
+//}  
+
+// 打印素数优化 
+//int main() {
+//	int i = 0;
+//	//sqrt 开平方的数学库函数 
+//	for(i = 100; i <= 200; i++){
+//		int j = 0;
+//		for(j = 2; j <= sqrt(i); j++){
+//			if(i % j == 0) {
+//				break;
+//			}
+//		} 
+//		if(j > sqrt(i)){
+//			//TO DO
+//			printf("%d ",i);
+//		}
+//	}
+//	return 0;
+//}
+
+
+
+//int main() {  
+//	int i = 0;
+//	int count = 0;
+//	for(int i=1;i<=100;i++){
+//		//TODO
+//		if(i % 10 == 9) {
+//			//个位是9 
+//			count++;
+//		}
+//		 if(i / 10 == 9){
+//			//TODO
+//			//十位是9 
+//			count++;
+//		}
+//	}
+//	printf("%d\n",count);
+//	return 0;
+//}
+
+
+
+//最大值
+//int main() {  
+//	int arr[] = {1,2,3,4,5,6,7,8,100,10};
+//	int max = arr[0]; //最大值
+//	int i = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	for(i=0;i < sz; i++){
+//		//TODO
+//		if(arr[i] > max){
+//			max  = arr[i];
+//		}
+//	} 
+//	printf("最大值是%d",max);
+//	return 0;
+//} 
+
+
+//打印乘法口诀表
+int main() {  
+	 for(int i=1;i<=9;i++){
+	 	//TODO
+	 	for(int j = 1; j <= i; j++) {
+		 	printf("%d x %d = %d\t",j,i,j*i );
+		 }
+		 printf("\n");
+	 } 
+	return 0;
+}  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
