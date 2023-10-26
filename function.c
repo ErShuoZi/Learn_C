9d192a9637369a2bfc736775a1fac6fa52ea46fc
ErShuoZi <1379768076@qq.com>
2023-10-26 14:04:20 +0800

    二分查找法

--- function.c
+++ function.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <math.h>
 //复制字符串 
 //int main(){
 //	char arr1[] = "haha";
@@ -38,21 +39,80 @@
 //}
 
 //指针类型int* 
-void Swap2(int* px,int* py) {
-	int tmp = 0;
-		tmp = *px;
-		*px = *py;
-		*py = tmp;
+//void Swap2(int* px,int* py) {
+//	int tmp = 0;
+//		tmp = *px;
+//		*px = *py;
+//		*py = tmp;
+//}
+//
+//int main() {
+//	int a = 10;
+//	int b = 20;
+//	int tmp = 0;
+//	printf("a = %d b = %d\n",a,b);
+//	Swap2(&a,&b);
+////	tmp = a;
+////	a = b;
+////	b = tmp;
+//	printf("a = %d b = %d\n",a,b);
+//}
+//是素数返回1 or 0 
+// isPrime(int n) {
+//	int j = 0;
+//	for(j = 2;j <= sqrt(n);j++){ 
+//		//TODO  
+//		if(n % j == 0){
+//			return 0;
+//		}
+//		return 1;
+//	} 
+//}
+//
+//int main() {
+//	int i = 0;
+//	for(i=100;i<=200;i++){
+//		//判断是否素数
+//	if(	isPrime(i)  == 1){
+//		//TODO
+//		printf("%d ",i);
+//	} 
+//	}
+//	return 0;
+//} 
+
+
+
+int binary_serch(int arr[],int k,int arrLength) {
+	//二分查找
+	int left = 0;
+	int right = arrLength - 1;
+	while(left <= right){
+		int middle = (left + right) / 2;
+		if(arr[middle] < k) {
+			left = middle + 1;
+		}
+		else if(arr[middle] > k) {
+			right = middle - 1;
+		}
+		else {
+			return middle;
+		}
+	} 
+	 return -1;
 }
 
+
 int main() {
-	int a = 10;
-	int b = 20;
-	int tmp = 0;
-	printf("a = %d b = %d\n",a,b);
-	Swap2(&a,&b);
-//	tmp = a;
-//	a = b;
-//	b = tmp;
-	printf("a = %d b = %d\n",a,b);
-}
+	int arr[] = {1,2,3,4,5,6,7,8,9,10};
+	int k = 17;
+	int arrLength = sizeof(arr) / sizeof(arr[0]);
+	int ret =  binary_serch(arr,k,arrLength);
+	if(ret == -1)  {
+		printf("找不到指定数字"); 
+	}
+	else {
+		printf("%d\n",ret);
+	}
+	return 0;
+} 
