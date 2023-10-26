90f5c198e12965067935d5405c128d9bebf5edf5
ErShuoZi <1379768076@qq.com>
2023-10-26 12:27:21 +0800

    函数交换两个值

--- function.c
+++ function.c
@@ -18,14 +18,41 @@
 
 //打印99乘法表
  
- void main() {
- 	//
- 	for(int i= 1;i<= 9;i++){
-	 	//TODO
-	 	for(int j =	1;j <= i;j++){
-		 	//TODO
-		 	printf("%d x %d = %d\t",j,i,j*i);
-		 }
-		 printf("\n");
-	 }
- } 
+// void main() {
+// 	//
+// 	for(int i= 1;i<= 9;i++){
+//	 	//TODO
+//	 	for(int j =	1;j <= i;j++){
+//		 	//TODO
+//		 	printf("%d x %d = %d\t",j,i,j*i);
+//		 }
+//		 printf("\n");
+//	 }
+// } 
+
+//void Swap1(int x,int y) {
+//	int tmp = 0;
+//		tmp = x;
+//		x = y;
+//		y = tmp;
+//}
+
+//指针类型int* 
+void Swap2(int* px,int* py) {
+	int tmp = 0;
+		tmp = *px;
+		*px = *py;
+		*py = tmp;
+}
+
+int main() {
+	int a = 10;
+	int b = 20;
+	int tmp = 0;
+	printf("a = %d b = %d\n",a,b);
+	Swap2(&a,&b);
+//	tmp = a;
+//	a = b;
+//	b = tmp;
+	printf("a = %d b = %d\n",a,b);
+}
