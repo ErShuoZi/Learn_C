a7e008798b1950c74f6b2ad1a6ea28f245387194
ErShuoZi <1379768076@qq.com>
2023-10-27 21:04:51 +0800

    递归函数

---
+++ recursion.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+//ตÝน้ 
+//int main() {
+//	printf("hehe\n") ;
+//	main();
+//	return 0;
+//} 
+void print(int n) {
+	if(n > 9) {
+		print(n/10);
+	}
+	printf("%d ",n%10);
+}
+
+int main() {
+	unsigned int num = 0;
+	scanf("%d",&num);
+	print(num);
+	return 0;
+} 
