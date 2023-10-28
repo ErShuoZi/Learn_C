bd0988a3eabb7563e6c51182bad97a31cb54dabc
ErShuoZi <1379768076@qq.com>
2023-10-28 13:16:21 +0800

    求字符串长度，不借用临时变量

--- recursion.c
+++ recursion.c
@@ -7,16 +7,40 @@
 //	main();
 //	return 0;
 //} 
-void print(int n) {
-	if(n > 9) {
-		print(n/10);
+//void print(int n) {
+//	if(n > 9) {
+//		print(n/10);
+//	}
+//	printf("%d ",n%10);
+//}
+//
+//int main() {
+//	unsigned int num = 0;
+//	scanf("%d",&num);
+//	print(num);
+//	return 0;
+//} 
+
+int my_strlen(char* str) {
+	if(*str != '\0') {
+		return 1 + my_strlen(str + 1);
 	}
-	printf("%d ",n%10);
+	else {
+		return 0;
+	}
 }
 
+//递归，大事化小
+//my_strlen("bit")
+//1 + my_strlen("it")
+//1 + 1 + my_strlen("t"); 
+//1 + 1 + 1 + my_strlen("")
+// 1+1+1+0 = 3 
 int main() {
-	unsigned int num = 0;
-	scanf("%d",&num);
-	print(num);
+	char arr[] = "bit";
+	
+	//求字符串长度(不创建临时变量)
+	int len = my_strlen(arr);//arr是数组，数组传参，传过去的不是一整个数组，而是第一个元素的地址 
+	printf("len = %d\n",len);
 	return 0;
 } 
