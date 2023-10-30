ac517616b406751a411f80a0dc23559ca04f9566
ErShuoZi <1379768076@qq.com>
2023-10-30 21:22:57 +0800

    冒泡排序

--- Array.c
+++ Array.c
@@ -1,10 +1,30 @@
+//一维数组 
+//int main() {
+//	int arr[] = {1,2,3,4,5,6,7,8,9,10};
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	
+//	for(int i=0;i<sz;i++){
+//		printf("&arr[%d] = %p\n",i,&arr[i]);
+//	}
+//	return 0;
+//} 
 
+//二维数组
 int main() {
-	int arr[] = {1,2,3,4,5,6,7,8,9,10};
-	int sz = sizeof(arr) / sizeof(arr[0]);
+	int arr[3][4] = {{1,2,3},{4,5}};
+	//	int arr[3][4] = {{1,2,3},{4,5}};
+	//	char ch[3][5];
+	//error
+	//int arr[][] ={1,2,3,4,5,6,7,8};
+	//二维数组下标不可省略列 ，行可以省略  
+//	int arr[][4] ={{1,2,3,4},{5,6,7,8}};
+
+	for(int i=0;i<3;i++){
+		for(int j=0;j<4;j++){
+		printf("&arr[%d][%d] = %p\n",i,j,&arr[i][j]);  
+		}
+		printf("\n");
+	} 
 	
-	for(int i=0;i<sz;i++){
-		printf("&arr[%d] = %p\n",i,&arr[i]);
-	}
 	return 0;
-} 
+}  
